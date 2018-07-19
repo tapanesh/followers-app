2c1d8a4801fe207df606d7ca9e0fb182f8716a89
Jyoti Behera <jbehera@teksystems.com>
2018-07-19 19:06:41 +0530

    first commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
+import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  backgroundColor = environment.navBarBackgroundColor;
 
   constructor() { }
 
